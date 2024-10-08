452267d2f04eb0be56e57f8d62102d7366ec07f0
fireball2024 <nolanpbeatty@gmail.com>
2024-10-08 15:32:45 -0400

    Create ClimberClimb.java

---
+++ src/main/java/frc/robot/commands/ClimberClimb.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class ClimerClimb extends Command {
+  private final ClimberSubsystem m_climb;
+  private double m_speed;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param  The subsystem used by this command.
+   */
+  public ClimberClimb(ClimberSubsystem climbSub, double speed) {
+    m_climb = climbSub;
+    m_speed = speed;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(ClimberSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_climb.climbRun(m_speed);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_climb.climbRun(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
